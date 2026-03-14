bc60f215e73cd9f41244626b23781862eb997f4d
Nisarg <nisargvp2004@gmail.com>
2026-03-14 20:18:32 +0530

    f

--- src/app/whatsapp-admin/webhook-debug/page.tsx
+++ src/app/whatsapp-admin/webhook-debug/page.tsx
@@ -1,28 +1,5 @@
-'use client';
+import WebhookDebugClient from './WebhookDebugClient';
 
-import { WhatsAppAdminProvider, useWhatsAppAdminContext } from '@/modules/whatsapp-admin/context/WhatsAppAdminProvider';
-import AuthSection from '@/modules/whatsapp-admin/components/AuthSection';
-import LoadingSection from '@/modules/whatsapp-admin/components/LoadingSection';
-import { WebhookDebugContent } from '@/modules/whatsapp-admin/components/WebhookDebugContent';
-
-function WebhookDebugPageContent() {
-  const { isAuthenticated, isLoading } = useWhatsAppAdminContext();
-
-  if (isLoading) {
-    return <LoadingSection />;
-  }
-
-  if (!isAuthenticated) {
-    return <AuthSection />;
-  }
-
-  return <WebhookDebugContent />;
-}
-
-export default function WebhookDebugPage() {
-  return (
-    <WhatsAppAdminProvider>
-      <WebhookDebugPageContent />
-    </WhatsAppAdminProvider>
-  );
+export default function Page() {
+  return <WebhookDebugClient />;
 }
